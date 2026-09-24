1860127edc4d3b5370192f3f6f5cbc06201b71c3
unknown <tot.zol0226@gmail.com>
2026-09-24 11:33:30 +0200

    update clues

--- src/data/clues.js
+++ src/data/clues.js
@@ -1,8 +1,8 @@
 export const clues = {
   zeus: {
     station: 1,
-    clue: "Az Olümposz királya villámot tart a kezében. Ki ő?",
-    answers: ["zeusz", "zeus"],
+    clue: "Mi a dalocska neve, ami úgy kezdődik, hogy: Erdős réteken…",
+    answers: ["Ibolya", "ibolya"],
     letter: "I",
     nextClue:
       "Szép alkotás a csendélet, nézzük meg hol van Erzsébet."
@@ -10,8 +10,8 @@ export const clues = {
 
   athena: {
     station: 2,
-    clue: "Ki volt a bölcsesség és a stratégiai hadviselés görög istennője?",
-    answers: ["athéné", "athene", "athena"],
+    clue: "Ki játszott a GT második napján elektronikus zenét?",
+    answers: ["Solar", "solar"],
     letter: "S",
     nextClue:
       "Elege van gyerekekből, hátat fordít nekik egyből."
@@ -19,8 +19,8 @@ export const clues = {
 
   poseidon: {
     station: 3,
-    clue: "Melyik görög isten uralkodott a tengerek felett?",
-    answers: ["poszeidón", "poszeidon", "poseidon"],
+    clue: "Ki a legalacsonyabb BTK Hök-ös? (Üdv, …)",
+    answers: ["Timi", "timi"],
     letter: "T",
     nextClue:
       "A járdán, mely keresztül szeli a parkot, indulj el a hungi irányába. Jobbra van egy tábla. Nagy kócsag van rajta, óvd a környezetet tesa!"
@@ -28,8 +28,8 @@ export const clues = {
 
   hades: {
     station: 4,
-    clue: "Ki volt az alvilág uralkodója a görög mitológiában?",
-    answers: ["hadész", "hades"],
+    clue: "Mi azoknak az italoknak a gyűjtőneve, melyek pokolian életben tartanak a GT alatt, szárnyakat adnak, és szörnyen nagy állatok leszünk tőlük?",
+    answers: ["Energiaital", "energiaital"],
     letter: "E",
     nextClue:
       "Téglafal mellett két szép csaj, az utolsó kódot a fűben keresd majd."
@@ -37,8 +37,8 @@ export const clues = {
 
   apollo: {
     station: 5,
-    clue: "Ki volt többek között a nap, a zene és a költészet görög istene?",
-    answers: ["apollón", "apollon", "apollo"],
+    clue: "Mi volt a 2025-ös GT himnusza?",
+    answers: ["Nintendo", "nintendo"],
     letter: "N",
     nextClue: null
   }
